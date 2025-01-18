e9466ce57aaf7920daf9d5986a1e75b17991ba11
Maxence Perrin <maxence@quick.agency>
2025-01-18 19:29:34 +0100

    Update vitest config to run headless

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from "vite";
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  test:{
+  test: {
     environment: "happy-dom",
     browser: {
       enabled: true,
       provider: "playwright",
+      headless: true,
       instances: [{ browser: "chromium" }],
     },
-  }
-})
+  },
+});
